bce699903dc3b7182f2a4590a7e41e330543e4bc
banana-galaxy <cawasp@gmail.com>
2020-06-18 20:57:30 -0700

    renamed cases variable to testcases

--- challenge6(hotel)/testcases.py
+++ challenge6(hotel)/testcases.py
@@ -2,7 +2,7 @@ import random
 
 
 def gen(amount=100000):
-    cases = []
+    testcases = []
     for o in range(amount):  # amount of test cases
         print(f"generating cases {o + 1}/{amount}", end="\r")
         case = []
@@ -13,8 +13,8 @@ def gen(amount=100000):
             for room in range(width):
                 case[i].append(
                     random.choice([0, random.randint(1, 100), random.randint(1, 1000), random.randint(1, 10000)]))
-        cases.append(case)
-    return cases
+        testcases.append(case)
+    return testcases
 
 
 if __name__ == "__main__":
